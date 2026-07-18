fd6ee1b43763ed277660f8e791a81b4447ae8c75
Vodloza-ctrl <junzatv@gmail.com>
2026-07-18 17:47:23 +0200

    still testing

---
+++ .wrangler/tmp/bundle-RVlg0a/middleware-insertion-facade.js
@@ -0,0 +1,10 @@
+				import worker, * as OTHER_EXPORTS from "C:\\Users\\Ya-Sibo Media\\zvakho_landing_page\\src\\index.js";
+				import * as __MIDDLEWARE_0__ from "C:\\Users\\Ya-Sibo Media\\zvakho_landing_page\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+
+				export * from "C:\\Users\\Ya-Sibo Media\\zvakho_landing_page\\src\\index.js";
+
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default
+				]
+				export default worker;
