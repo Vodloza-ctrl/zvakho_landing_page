ed821271e5180058812d6a5c195986eb2c10eb37
Vodloza-ctrl <junzatv@gmail.com>
2026-07-18 17:21:21 +0200

    implement changes

--- .wrangler/tmp/dev-qX5ILQ/index.js
+++ .wrangler/tmp/dev-qX5ILQ/index.js
@@ -1,7 +1,7 @@
 var __defProp = Object.defineProperty;
 var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
 
-// .wrangler/tmp/bundle-H2Gtl8/checked-fetch.js
+// .wrangler/tmp/bundle-iUTo98/checked-fetch.js
 var urls = /* @__PURE__ */ new Set();
 function checkURL(request, init) {
   const url = request instanceof URL ? request : new URL(
@@ -27,7 +27,7 @@ globalThis.fetch = new Proxy(globalThis.fetch, {
   }
 });
 
-// .wrangler/tmp/bundle-H2Gtl8/strip-cf-connecting-ip-header.js
+// .wrangler/tmp/bundle-iUTo98/strip-cf-connecting-ip-header.js
 function stripCfConnectingIPHeader(input, init) {
   const request = new Request(input, init);
   request.headers.delete("CF-Connecting-IP");
@@ -580,7 +580,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-H2Gtl8/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-iUTo98/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -612,7 +612,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-H2Gtl8/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-iUTo98/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
